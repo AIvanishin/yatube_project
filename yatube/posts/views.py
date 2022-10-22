49df9cca21d1d67cce9a93ecede4fed0d779a765
Андрей Иванишин <AIvanishin.kld@yandex.ru>
2022-10-22 10:57:25 +0200

    селекторы и рендер

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+def group_posts(request, slug):
+    return HttpResponse('Посты группы')    
